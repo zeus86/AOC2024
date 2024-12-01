cbf7c37d15bbd587466627811574151152ddbbd3
zeus86 <zeus_86@gmx.de>
2024-12-01 15:53:46 +0100

    gitignore

--- 01/src/code.sh
+++ 01/src/code.sh
@@ -39,4 +39,33 @@ echo "stage one final result is:"
 cat $INPUTDIR/$INPUTFILE.distance_sum_01
 }
 
+stage2 () {
+echo -e "stage 2.1: the first steps are the same: create left and light lists in sorted form"
+cat $INPUT | tr -s " " | cut -f 1 -d " " > $INPUTDIR/$INPUTFILE.split_left_02
+cat $INPUTDIR/$INPUTFILE.split_left_02 | sort > $INPUTDIR/$INPUTFILE.split_left_sorted_02
+cat $INPUT | tr -s " " | cut -f 2 -d " " > $INPUTDIR/$INPUTFILE.split_right_02
+cat $INPUTDIR/$INPUTFILE.split_right_02 | sort > $INPUTDIR/$INPUTFILE.split_right_sorted_02
+echo "done."
+
+echo -e "stage 2.2: count values in second list, and add them bc-friendly"
+> $INPUTDIR/$INPUTFILE.combined_sorted_similarity_02
+while read -r line
+do
+	SIMILARITY=$(grep ^$line$ $INPUTDIR/$INPUTFILE.split_right_sorted_02 | wc -l || echo "0")
+	# echo "line $line sim $SIMILARITY"
+	# we need only similarities >0
+	[[ $SIMILARITY -gt 0 ]] && echo "$line * $SIMILARITY" >> $INPUTDIR/$INPUTFILE.combined_sorted_similarity_02
+	# also write them down in calculated form for bc
+	[[ $SIMILARITY -gt 0 ]] && echo "$line * $SIMILARITY" | bc >> $INPUTDIR/$INPUTFILE.combined_sorted_similarity_bc_02
+done < $INPUTDIR/$INPUTFILE.split_left_sorted_02
+
+echo "stage 2.3: add up the values"
+cat $INPUTDIR/$INPUTFILE.combined_sorted_similarity_bc_02 | paste -sd+ | bc > $INPUTDIR/$INPUTFILE.similarity_sum_02
+echo "done."
+
+echo "stage two final result is:"
+cat $INPUTDIR/$INPUTFILE.similarity_sum_02
+}
+
+
 $1
